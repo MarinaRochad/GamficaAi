bd27c3b9daa6f294944bf766f24d4349e3529a07
MARINA ROCHA DA SILVA <47079439850@info.local>
2024-05-24 10:49:48 -0300

    olar

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ let iconeX = document.getElementById("icone-x")
 
 
 function abreFechaMenu() {
-    
+
     //Menu Fechado - tem a classe menu fechado
     //Menu aberto - nao tem a classe menu fechado 
 
     //se o menu contem a classe menu-fechado
-    if (menu.classList.contains("menu-fechado")){
+    if (menu.classList.contains("menu-fechado")) {
         //abrir o menu- tirar a classe menu-fechado
         menu.classList.remove("menu-fechado")
 
@@ -62,7 +62,7 @@ let slideAtual = 0
 
 banner.classList.add(slides[slideAtual])
 
-function mostrarProximoSlide(){
+function mostrarProximoSlide() {
     // remover o slide anterior
     banner.classList.remove(slides[slideAtual])
 
@@ -71,4 +71,63 @@ function mostrarProximoSlide(){
 
     //mostrar slide de acordo com o slideAtual
     banner.classList.add(slides[slideAtual])
+}
+
+function mostrarSlideAnterior() {
+    // Remover o slide anterior
+    banner.classList.remove(slides[slideAtual])
+
+    if (slideAtual > 0) {
+        // Subtrair 1 na variavel slideAtual
+        slideAtual--
+    } else {
+        // Voltar para o ultimo slide
+        slideAtual = 2
+    }
+
+    // Mostrar slide de acordo com o slide atual
+    banner.classList.add(slides[slideAtual])
+}
+
+function selecionarSlide(indiceSlide) {
+    // Remove o slide atual
+    banner.classList.remove(slides[slideAtual])
+
+    // Atualiza a variavel com o indice de slide selecionado
+    slideAtual = indiceSlide
+
+    // Mostra o slide selecionado e salvo na variavel slideAtual
+    banner.classList.add(slides[slideAtual])
+}
+
+
+// Carregamento dinamico dos cases
+let listaCases = [
+    
+
+]
+
+function renderizarCases() {
+    //Encontrar o elemento para inserir os cards
+    let containerCards = document.querySelector(".container-cards")
+
+    //variavel para guardar o html dos cases montados 
+    let template = ""
+    //Para cada case da listaCases
+    listaCases.forEach(cardCase => {
+        //Montar o html do card, passando os atributos do case 
+        template += `<div class="card">
+        <img src=${ cardCase.imagem } alt="">
+        <p> ${ cardCase.descricao }</p>
+        <button>Ver mais</button>
+      </div>`
+
+
+    })
+
+
+
+
+    //Inserir html dos cases montados no elemento container-cards
+    containerCards.innerHTML = template 
 }
